68043154fb9d719893f1db53c4990bab53f1e087
horvatos <os.torn@gmail.com>
2023-10-07 23:15:14 +0200

    Addning application form

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/horvatos/bookings/internal/config"
 	"github.com/horvatos/bookings/internal/handlers"
+	"github.com/horvatos/bookings/internal/models"
 	"github.com/horvatos/bookings/internal/render"
 )
 
@@ -19,6 +21,8 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
+	//what am i going to put in session
+	gob.Register(models.Reservation{})
 
 	//change this to true when in production
 	app.InProduction = false
